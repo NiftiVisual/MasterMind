06ac896f16b35e473beabb33d1d3c15ea0667ea5
NiftiVisual <79956817+NiftiVisual@users.noreply.github.com>
2021-04-09 16:45:33 +0400

    Add files via upload

--- mastermind python.py
+++ mastermind python.py
@@ -5,7 +5,7 @@ from random import * #on importe la librairie random pour pouvoir avoir un code 
    MasterMind niv= nombres de 
    chiffres dans le code 
    coul = chiffre de 1à6'''
-def MasterMind(niv= 4, coul = 6) 
+def MasterMind(niv= 4, coul = 6): 
     
     #nombre de tentatives qui augmentera au fil des tours
     c = 1 
@@ -16,6 +16,17 @@ def MasterMind(niv= 4, coul = 6)
         j= list(input("Coup "+ str(c) + " : "))  #le code a retrouver
         if len(j) == 0: return 'PERDU !' + "".join(secret) #si la réponse du joueur est vide = perdu
         c+=1
-        bien; mal = 0, 0
-        for i, in enumerate(j):
+        bien, mal = 0, 0
+        for i, v in enumerate(j):
             if v == code[i]:
+                bien += 1
+                j[i] = "#"
+                code[i] = "*"
+        if bien == 4: return "GAGNE ! Tu es vraiment un MASTERMIND"
+        for i,v in enumerate(j):
+            if v in code:
+                mal += 1
+                code[code.index(v)] = "*"
+        print("Bien: {}, Mal: {}".format(bien,mal))
+        
+    
